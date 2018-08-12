64ce28b72b8719a440d435e491987140eacc5f08
Marius <marius@twostairs.com>
2018-08-12 19:58:52 +0200

    Fix for unknown logger method

--- src/ServiceProviders/Database/Drivers/Cql/index.js
+++ src/ServiceProviders/Database/Drivers/Cql/index.js
@@ -29,7 +29,12 @@ module.exports = class CqlDriver extends Driver {
 
         this._client = new cql.Client(this._connection);
         this._client.on('log', (level: string, className: string, message: string, furtherInfo: any) => {
-            const loggerMethod = (level === 'verbose' ? 'debug' : level);
+            let loggerMethod = level;
+
+            if(this.logger.hasOwnProperty(level) === false) {
+                loggerMethod = 'debug';
+            }
+
             // @flowIgnore TODO: Fix this
             this.logger[loggerMethod]('Database: (CqlDriver) (%s) %s %j', className, message, furtherInfo);
         });
