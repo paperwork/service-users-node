9eca647c21ce1380a8d8dc0f7fe577af771abfe5
Marius <marius@twostairs.com>
2018-08-12 19:59:10 +0200

    Draft implementation of getUser

--- src/Collections/User/Cql.js
+++ src/Collections/User/Cql.js
@@ -1,6 +1,19 @@
 //@flow
 
 const PaperworkCollection = require('paperwork-sdk-service-node/lib/PaperworkCollection');
+const Common = require('paperframe/lib/Common');
 
 module.exports = class UserCql extends PaperworkCollection {
+    async getUser(username: string): Promise<?Object> {
+        const query = 'SELECT * FROM users WHERE username = ?';
+        const result: Object = await this.dbc.execute(query, [username]);
+
+        if(result.hasOwnProperty('rows')
+        && Array.isArray(result.rows)
+        && result.rows === Common.ONE) {
+            return result.rows[Common.FIRST];
+        }
+
+        return null;
+    }
 };
