7513cb5a438af48b2d1ce7f7c78a4f7ba021515f
Marius <marius@twostairs.com>
2018-08-18 11:44:01 +0200

    Added dbc type definition, replaced results.row by results.first()

--- src/Collections/User/Cql.js
+++ src/Collections/User/Cql.js
@@ -4,16 +4,11 @@ const PaperworkCollection = require('paperwork-sdk-service-node/lib/PaperworkCol
 const Common = require('paperframe/lib/Common');
 
 module.exports = class UserCql extends PaperworkCollection {
+    dbc:                Function
+
     async getUser(username: string): Promise<?Object> {
         const query = 'SELECT * FROM users WHERE username = ?';
         const result: Object = await this.dbc.execute(query, [username]);
-
-        if(result.hasOwnProperty('rows')
-        && Array.isArray(result.rows)
-        && result.rows === Common.ONE) {
-            return result.rows[Common.FIRST];
-        }
-
-        return null;
+        return result.first();
     }
 };
