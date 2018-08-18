317c966d701941424f45ff1af80ff5655fafdb58
Marius <marius@twostairs.com>
2018-08-19 00:31:58 +0200

    Async support

--- src/ServiceProviders/Messaging/index.js
+++ src/ServiceProviders/Messaging/index.js
@@ -14,8 +14,7 @@ module.exports = class MessagingServiceProvider extends ServiceProvider {
     _natsConnection:            string
     _nats:                      ?NATS
 
-    initialize(): boolean {
-        // Kong API URL
+    async initialize(): Promise<boolean> {
         this._natsConnection = this.getEnv('NATS_CONNECTION');
         if(typeof this._natsConnection === 'undefined') {
             this.logger.error('MessagingServiceProvider: Could not initialize, NATS_CONNECTION not set!');
@@ -23,20 +22,28 @@ module.exports = class MessagingServiceProvider extends ServiceProvider {
             this._nats = null;
         } else {
             this._nats = NATS.connect(this._natsConnection, this._serverName);
-            this._nats.on('connect', () => {
+
+            return new Promise((fulfill, reject) => {
                 if(typeof this._nats === 'undefined'
                 || this._nats === null) {
-                    return false;
+                    return fulfill(false);
                 }
 
-                let eventsSubscription = this._nats.subscribe('*', async (message: Object, reply: Object, subject: string) => {
-                    this._processMessage(message, reply, subject);
+                return this._nats.on('connect', () => {
+                    if(typeof this._nats === 'undefined'
+                    || this._nats === null) {
+                        this.logger.debug('MessagingServiceProvider: Could not initialize with NATS_CONNECTION set to %s.', this._natsConnection);
+                        return fulfill(false);
+                    }
+
+                    let eventsSubscription = this._nats.subscribe('*', async (message: Object, reply: Object, subject: string) => {
+                        this._processMessage(message, reply, subject);
+                    });
+
+                    this.logger.debug('MessagingServiceProvider: Initialized with NATS_CONNECTION set to %s.', this._natsConnection);
+                    return fulfill(true);
                 });
-
-                return true;
             });
-
-            this.logger.debug('MessagingServiceProvider: Initialized with NATS_CONNECTION set to %s.', this._natsConnection);
         }
 
         return true;
